8f37d7dac1e76f1ebb0bdc7cd3209f91e95d8c6e
radhakrishnan k <radhakrishnanjb053@gmail.com>
2026-03-22 20:09:51 +0530

    final updates

--- tastybites/settings.py
+++ tastybites/settings.py
@@ -169,3 +169,10 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+#white noise
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = True

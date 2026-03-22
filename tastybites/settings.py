f082c5b9008aaf7c00d29556cd069f837b1f0fbf
radhakrishnan k <radhakrishnanjb053@gmail.com>
2026-03-22 19:29:23 +0530

    updates

--- tastybites/settings.py
+++ tastybites/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+ALLOWED_HOSTS=[ 
+    "onrender.com",
+    "localhost,127.0.0.1"
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.onrender.com",
